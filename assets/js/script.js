b7811437351526ecf8f525f2507e0b0201f3da87
xmccopy <comacmccopy@gmail.com>
2024-06-11 09:27:30 +0900

    chore : top page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 let slideIndex = 0;
+let slideIndex1 = 0
 showSlides();
 
 // Next-previous control
 function nextSlide() {
   slideIndex++;
+  slideIndex1++;
   showSlides();
   timer = _timer; // reset timer
 }
 
 function prevSlide() {
   slideIndex--;
+  slideIndex1--;
   showSlides();
   timer = _timer;
 }
@@ -17,30 +20,43 @@ function prevSlide() {
 // Thumbnail image controlls
 function currentSlide(n) {
   slideIndex = n - 1;
+  slideIndex1= n - 1;
   showSlides();
   timer = _timer;
 }
 
 function showSlides() {
   let slides = document.querySelectorAll(".mySlides");
+  let slides1 = document.querySelectorAll(".mySlides1");
   let dots = document.querySelectorAll(".dots");
 
-  if (slideIndex > slides.length - 1) slideIndex = 0;
-  if (slideIndex < 0) slideIndex = slides.length - 1;
+  if ((slideIndex > slides.length - 1) && (slideIndex1 > slides1.length - 1)) {
+    slideIndex = 0;
+    slideIndex1 = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+    slideIndex = slides1.length - 1;
+  }
   
   // hide all slides
   slides.forEach((slide) => {
     slide.style.display = "none";
   });
+  slides1.forEach((slide) => {
+    slide.style.display = "none";
+  });
   
   // show one slide base on index number
   slides[slideIndex].style.display = "block";
+  slides1[slideIndex1].style.display = "block";
   
   dots.forEach((dot) => {
     dot.classList.remove("active");
   });
   
   dots[slideIndex].classList.add("active");
+  dots[slideIndex1].classList.add("active");
 }
 
 // autoplay slides --------
